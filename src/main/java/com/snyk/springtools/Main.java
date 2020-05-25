6e99b66775a3e1c831fab74db170627807a72ce6
Michael Komraz <mikomraz@gmail.com>
2020-05-25 17:10:01 +0300

    change to ContextRefreshedEvent

--- src/main/java/com/snyk/springtools/Main.java
+++ src/main/java/com/snyk/springtools/Main.java
@@ -1,8 +1,8 @@
 package com.snyk.springtools;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 
 public class Main {
 
@@ -11,10 +11,12 @@ public class Main {
 
         SpringApplication application = new SpringApplication(originalApp);
 
-        application.addListeners((ApplicationListener<ApplicationStartedEvent>) event -> {
+        application.addListeners((ApplicationListener<ContextRefreshedEvent>) event -> {
             ContextUtils.printBeansInContext(event.getApplicationContext());
             System.exit(0);
         });
+
+        application.setLogStartupInfo(false);
 
         application.run(args);
     }
